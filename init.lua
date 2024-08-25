b39e6fd5c07159b390592a8630bf535dd3a937fe
Atlante <108891261+Atlante1952@users.noreply.github.com>
2024-08-25 08:33:54 +0200

    Fixed sprint speed bug when you already have a speed effect
    Fixed sprint speed bug when you already have a speed effect

--- init.lua
+++ init.lua
@@ -1,17 +1,24 @@
+local player_base_speeds = {}
+
 local function set_speed(player)
+    local name = player:get_player_name()
     local controls = player:get_player_control()
     local current_physics = player:get_physics_override()
-    local current_speed = current_physics.speed or 1.0
+    local base_speed = player_base_speeds[name] or 1.0
 
     if controls.aux1 then
-        player:set_physics_override({speed = current_speed + 0.8})
+        player:set_physics_override({speed = base_speed + 0.9})
     else
-        player:set_physics_override({speed = current_speed})
+        player:set_physics_override({speed = base_speed})
     end
 end
 
 minetest.register_globalstep(function(dtime)
     for _, player in ipairs(minetest.get_connected_players()) do
+        local name = player:get_player_name()
+        if not player_base_speeds[name] then
+            player_base_speeds[name] = player:get_physics_override().speed or 1.0
+        end
         set_speed(player)
     end
 end)
